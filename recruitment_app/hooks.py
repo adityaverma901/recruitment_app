b9030560f790d6b39834115a474d73f4a7a5007a
adii-gen <aditya.verma@gennextit.co.in>
2025-10-14 13:26:55 +0530

    added daily dashboard cron

--- recruitment_app/hooks.py
+++ recruitment_app/hooks.py
@@ -33,6 +33,20 @@ doc_events = {
 }
 override_doctype_class = {
     "Company": "recruitment_app.unique_abbr.CustomCompany"
+},
+scheduler_events = {
+    # Run daily at 11:59 PM
+    "cron": {
+        # Testing: Run every 2 minutes
+        "*/1 * * * *": [
+            "recruitment_app.daily_dashboard_logs.daily_api_data_sync"
+        ]
+        
+        # Production: Uncomment below and comment above after testing
+        # "59 23 * * *": [
+        #     "recruitment_app.tasks.daily_api_data_sync"
+        # ]
+    }
 }
 
 # Each item in the list will be shown as an app in the apps page
